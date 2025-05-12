b8ff73b243f149d0f60a40c9473197a96875c888
Jinwoo Lee <jinlee1703@gmail.com>
2025-05-12 14:41:14 +0900

    chore: 프로젝트 초기 설정 #1
    Co-authored-by: 진서연 <22020306jinseoyeon@gmail.com>

---
+++ src/main/java/kr/co/csalgo/CsAlgoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.csalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsAlgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsAlgoApplication.class, args);
+    }
+
+}
