f235bc4a10e76adc2a79a110b999a8f8901fe943
wlstj <22020306jinseoyeon@gmail.com>
2025-05-13 11:08:39 +0900

    fix: soft delete 적용 #3
    Co-authored-by: Jinwoo Lee <jinlee1703@gmail.com>

--- src/main/java/kr/co/csalgo/domain/common/entity/IdentifiableEntity.java
+++ src/main/java/kr/co/csalgo/domain/common/entity/IdentifiableEntity.java
@@ -1,13 +1,12 @@
 package kr.co.csalgo.domain.common.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class IdentifiableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
