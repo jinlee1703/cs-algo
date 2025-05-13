c661d3bd189196fed1208da2f7d423d773d4998a
wlstj <22020306jinseoyeon@gmail.com>
2025-05-13 10:35:26 +0900

    feat: question entity 생성 #3
    Co-authored-by: Jinwoo Lee <jinlee1703@gmail.com>

---
+++ src/main/java/kr/co/csalgo/domain/question/entity/Question.java
@@ -0,0 +1,45 @@
+package kr.co.csalgo.domain.question.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String solution;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public Question(String title, String description, String solution, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        this.title = title;
+        this.description = description;
+        this.solution = solution;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+}
