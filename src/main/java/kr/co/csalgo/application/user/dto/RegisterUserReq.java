a41aafab8beefd3f8bd06dafcd9222c3fa69f5eb
wlstj <22020306jinseoyeon@gmail.com>
2025-05-15 17:06:51 +0900

    test: 사용자 구독 test code 작성 #8

--- src/main/java/kr/co/csalgo/application/user/dto/RegisterUserReq.java
+++ src/main/java/kr/co/csalgo/application/user/dto/RegisterUserReq.java
@@ -2,6 +2,7 @@ package kr.co.csalgo.application.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -9,4 +10,9 @@ public class RegisterUserReq {
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @Builder
+    public RegisterUserReq(String email) {
+        this.email = email;
+    }
 }
---
+++ src/test/java/kr/co/csalgo/presentation/user/UserControllerTest.java
@@ -0,0 +1,87 @@
+package kr.co.csalgo.presentation.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import kr.co.csalgo.application.user.dto.RegisterUserReq;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    @DisplayName("사용자는 정상 이메일로 구독할 수 있다.")
+    void createSuccess() throws Exception {
+        RegisterUserReq registerUserReq = RegisterUserReq.builder()
+                .email("team-jjins@gmail.com")
+                .build();
+
+        mockMvc.perform(post("/api/subscriptions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(registerUserReq)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("중복된 이메일로는 구독할 수 없다.")
+    void duplicateEmail() throws Exception {
+        RegisterUserReq registerUserReq = RegisterUserReq.builder()
+                .email("duplicate@gmail.com")
+                .build();
+
+        mockMvc.perform(post("/api/subscriptions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(registerUserReq)));
+
+        mockMvc.perform(post("/api/subscriptions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(registerUserReq)))
+                .andExpect(status().isConflict())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("이메일이 공백이면 구독할 수 없다.")
+    void blankEmail() throws Exception {
+        RegisterUserReq registerUserReq = RegisterUserReq.builder()
+                .email("")
+                .build();
+
+        mockMvc.perform(post("/api/subscriptions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(registerUserReq)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 이메일 형식으로는 구독할 수 없다.")
+    void invalidateEmailFormat() throws Exception {
+        RegisterUserReq registerUserReq = RegisterUserReq.builder()
+                .email("team-jjinsgmail.com")
+                .build();
+
+        mockMvc.perform(post("/api/subscriptions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(registerUserReq)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+}
